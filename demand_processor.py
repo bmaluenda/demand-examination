f44a65dfab1290fc4d664526064bb84e5ab16c4f
Benjamin Maluenda <bmaluendap@gmail.com>
2017-04-06 16:50:46 -0300

    Initial commit with demand reader script.

---
+++ demand_processor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from datetime import datetime
+df = pd.read_excel("C:/Users/inodu_notebook_01/Desktop/Consumo Cristalerias 2014.xlsx")
+days=[31,28,31,30,31,30,31,31,30,31,30,31]
+dates=[]
+for m in [1,2,3,4,5,6,7,8,9,10,11,12]:
+     for d in range(1,days[m-1]+1):
+         for h in range(24):
+             dates.append(datetime(2014,m,d,h))
+for i in range(8760):
+    df.set_value(i,'datetime',dates[i])
+
+df.set_index('datetime',drop=True,inplace=True)
+DFlist = [group[1] for group in df.groupby(df.index.date)]
