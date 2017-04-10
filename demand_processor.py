5c837c63e9be79048b6edd5dc503df08ad795e43
Benjamin Maluenda <bmaluendap@gmail.com>
2017-04-10 12:07:09 -0300

    Implemented the SciPy hierarchichal clustering methods. The DTW distance is used as a distance metric and clustering is performed with the ward method. Representative curves for each cluster are plotted.

--- demand_processor.py
+++ demand_processor.py
@@ -1,7 +1,8 @@
+import numpy as np
 import pandas as pd
 from datetime import datetime
 from matplotlib import pyplot as plt
-import numpy as np
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 # DTW function created by bistaumanga
 # https://gist.github.com/bistaumanga/6023705
@@ -35,65 +36,40 @@ def DTW(A, B, w = 2, d = lambda x,y: (x-y)**2):
     path.append((0,0))
     return (cost[-1, -1])**0.5 #, path
 
-def LB_Keogh(s1,s2,r):
-    LB_sum=0
-    for ind,i in enumerate(s1):
-
-        lower_bound=min(s2[(ind-r if ind-r>=0 else 0):(ind+r)])
-        upper_bound=max(s2[(ind-r if ind-r>=0 else 0):(ind+r)])
-
-        if i>upper_bound:
-            LB_sum=LB_sum+(i-upper_bound)**2
-        elif i<lower_bound:
-            LB_sum=LB_sum+(i-lower_bound)**2
-
-    return (LB_sum**0.5)
-
-import random
-
-def k_means_clust(data,num_clust,num_iter,w):
-    centroids=random.sample(data,num_clust)
-    counter=0
-    for n in range(num_iter):
-        counter+=1
-        print counter
-        assignments={}
-        #assign data points to clusters
-        for ind,i in enumerate(data):
-            min_dist=float('inf')
-            closest_clust=None
-            for c_ind,j in enumerate(centroids):
-                if LB_Keogh(i,j,5)<min_dist:
-                    cur_dist=DTW(i,j,w)
-                    if cur_dist<min_dist:
-                        min_dist=cur_dist
-                        closest_clust=c_ind
-            if closest_clust in assignments:
-                assignments[closest_clust].append(ind)
-            else:
-                assignments[closest_clust]=[]
-        print assignments
-        #recalculate centroids of clusters
-        for key in assignments:
-            clust_sum=0
-            for k in assignments[key]:
-                clust_sum=clust_sum+data[k]
-            centroids[key]=[m/len(assignments[key]) for m in clust_sum]
-    return centroids
-
 df = pd.read_excel("C:/Users/inodu_notebook_01/Desktop/Consumo Cristalerias 2014.xlsx")
-days=[31,28,31,30,31,30,31,31,30,31,30,31]
+days_in_month=[31,28,31,30,31,30,31,31,30,31,30,31]
 dates=[]
-for m in [1,2,3,4,5,6,7,8,9,10,11,12]:
-     for d in range(1,days[m-1]+1):
+for m in range(1,13):
+     for d in range(1,days_in_month[m-1]+1):
          for h in range(24):
              dates.append(datetime(2014,m,d,h))
 for i in range(8760):
     df.set_value(i,'datetime',dates[i])
 
-df.set_index('datetime',drop=True,inplace=True)
+df.set_index('datetime', drop=True, inplace=True)
 DFlist = [group[1] for group in df.groupby(df.index.date)]
 data = np.empty((365,24))
 for ind, DF in enumerate(DFlist):
     data[ind] = np.array(DF.transpose())[0]
-centroids=k_means_clust(data,num_clust=2,num_iter=10,w=2)
+
+# the linkage function may receive custom distances in a compact vector format
+distance_vector = np.zeros((1,66430))
+a = 0
+for d1 in range(364):
+    for d2 in range(d1+1,365):
+        distance_vector[0][a] = DTW(data[d1], data[d2])
+        a += 1
+
+n_clust = 5
+Z=linkage(distance_vector[0], method='ward')
+dendrogram(Z, truncate_mode='lastp', n_clust=5, get_leaves=True)
+clusters = fcluster(Z, n_clust, criterion='maxclust')
+clusters_len = np.zeros((n_clust,1))
+rep_curves = np.zeros((n_clust,24))
+for c in range(n_clust):
+    for d in range(365):
+        if clusters[d] == c+1:
+            clusters_len[c] += 1
+            for j in range(24):
+                rep_curves[c][j] += data[d][j]
+    rep_curves[c] /= clusters_len[c]
